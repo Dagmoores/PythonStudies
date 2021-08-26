986adfd1ab026a8fadd502849053318965598d2f
Daniel Moore <dagmoores@gmail.com>
2021-08-26 13:39:32 -0300

    Update problema_pratico3-7.py

--- Projeto_Integrador_Estudos/problema_pratico3-7.py
+++ Projeto_Integrador_Estudos/problema_pratico3-7.py
@@ -1,1 +1,26 @@
+# Retirado do livro Introdução à Computação em Python - Um Foco no Desenolvimento de Aplicações - PERKOVIC, Ljubomir
 
+# Escreva um laço for que exiba a seguinte sequência de números, um por linha. (a)Inteiros de 3 até 12, inclusive este. (b)Inteiros de 0 até (mas não incluindo) 9, com um passo de 2 em vez do padrão 1 (isto é, 0, 2, 4, 6, 8). (c)Inteiros de 0 até (mas não incluindo) 24, com um passo de 3. (d)Inteiros de 3 até (mas não incluindo) 12, com um passo de 5.
+
+# Resolução (a)
+
+for i in range(3, 13):
+  print(i)
+print('')
+
+# Resolução (b)
+
+for i in range(0, 9, 2):
+  print(i)
+print('')
+
+# Resolução (c)
+
+for i in range(1, 25, 3):
+  print(i)
+print('')
+
+# Resolução (d)
+
+for i in range(4, 13, 5):
+  print(i)
