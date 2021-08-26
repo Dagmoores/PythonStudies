f96060781cb44e6122007a02e2758e8f6d1aaf22
Daniel Moore <dagmoores@gmail.com>
2021-08-26 13:33:17 -0300

    Update problema_pratico3-6.py

--- Projeto_Integrador_Estudos/problema_pratico3-6.py
+++ Projeto_Integrador_Estudos/problema_pratico3-6.py
@@ -1,1 +1,13 @@
+#Retirado do livro Introdução à Computação em Python - Um Foco no Desenolvimento de Aplicações - PERKOVIC, Ljubomir
 
+# Escreva o laço for que exibirá as sequências de números a seguir, um por linha, no shell interativo do Python. 
+# (a)Inteiros de 0 a 9 (isto é, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9). 
+# (b)Inteiros de 0 a 1 (isto é, 0, 1).
+
+# Resolução (a)
+for i in range(10):
+  print(i)
+
+# Resolução (b)
+for i in range(2):
+  print(i)
