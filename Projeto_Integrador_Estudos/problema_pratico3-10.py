82e85b7d77461f145bcb2945bb37b78cdf5420c2
Daniel Moore <dagmoores@gmail.com>
2021-08-26 14:12:59 -0300

    Create problema_pratico3-10.py

---
+++ Projeto_Integrador_Estudos/problema_pratico3-10.py
@@ -0,0 +1,18 @@
+# Retirado do livro Introdução à Computação em Python - Um Foco no Desenolvimento de Aplicações - PERKOVIC, Ljubomir
+
+# Escreva a função negativos(), que aceita uma lista como entrada e exibe, um por linha, os valores negativos na lista. A função não deverá retornar nada. >>> negatives([4, 0, -1, -3, 6, -9]) -1 -3 -9
+
+def negativos(input_list):
+
+  if type(input_list) == type(list()):
+
+    for i in input_list:
+      x = eval(i)
+      if type(x) == type(int()):
+        if i < 0:
+          print(i)
+
+  else:
+    print('Você deve digitar um tipo list')
+
+negativos([10, -4, -2, "-1"])
