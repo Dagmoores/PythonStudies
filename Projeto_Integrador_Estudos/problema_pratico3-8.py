8cc649c97300d4e0591f8379a24825c71872f2f7
Daniel Moore <dagmoores@gmail.com>
2021-08-26 13:49:06 -0300

    Create problema_pratico3-8.py

---
+++ Projeto_Integrador_Estudos/problema_pratico3-8.py
@@ -0,0 +1,6 @@
+# Retirado do livro Introdução à Computação em Python - Um Foco no Desenolvimento de Aplicações - PERKOVIC, Ljubomir
+
+# Defina, diretamente no shell interativo, a função média(), que aceita dois números como entrada e retorna a média dos números. Um exemplo de uso é: >>> average(2, 3.5) 2.75
+
+def f(x, y):
+  return (x + y) / 2
